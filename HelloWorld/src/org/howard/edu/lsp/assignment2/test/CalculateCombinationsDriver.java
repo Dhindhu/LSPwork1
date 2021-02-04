811617d940115f1ef9e767fc02411657b2330277
Dhindhu <chidhindhu@gmail.com>
2021-02-04 12:55:58 -0800

    Cleaned CalculateCombinationsDriver.java

--- HelloWorld/src/org/howard/edu/lsp/assignment2/test/CalculateCombinationsDriver.java
+++ HelloWorld/src/org/howard/edu/lsp/assignment2/test/CalculateCombinationsDriver.java
@@ -79,46 +79,6 @@ public class CalculateCombinationsDriver{
     	}
     }
 }
-/*
- * Inputed array: [1, 2, 3, 4, 5, 6]
- * Targeted sum: 6
- * All possible combinations below: 
- * [2, 1, 0]
- * [3, 1]
- * [4, 0]
- * [5]
- * 
- * Inputed array: [1, 2, 3, 4, 5, 6]
- * Targeted sum: 15
- * All possible combinations below: 
- * [4, 3, 2, 1, 0]
- * [5, 3, 2, 1]
- * [5, 4, 2, 0]
- * [5, 4, 3]
- * 
- * Inputed array: [6, 6]
- * Targeted sum: 15
- * All possible combinations below:
- * There are no combinations with sum 15
- * 
- * Inputed array: [0, 1, 5, 22]
- * Targeted sum: 12
- * All possible combinations below: 
- * There are no combinations with sum 12
- * 
- * Inputed array: [70, 11, 6, 22]
- * Targeted sum: 22
- * All possible combinations below: 
- * [3]
- * 
- * Inputed array: [6, 6, 6, 6]
- * Targeted sum: 6
- * All possible combinations below: 
- * [0]
- * [1]
- * [2]
- * [3]
- */
 
 /*
  * Chidindu Alim
